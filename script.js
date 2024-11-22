cde64ab51fd68ac4cd33212916075a2f927101d3
Eduardo Vieira <duduvieiradev@gmail.com>
2024-11-22 11:18:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function comprar() {
+    alert("Redirecionando para a página de pagamento...");
+    // Substitua pelo link da sua plataforma de checkout
+    window.location.href = "https://seucheckout.com";
+  }
+  
